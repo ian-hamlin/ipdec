07102738dc26c78fe06d7037d429eaf4d9e4496b
ian-hamlin <ian-hamlin@users.noreply.github.com>
2018-01-04 18:38:44 +0000

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![feature(i128_type)]
+
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
+use std::env;
+use std::process::exit;
+
+fn main() {
+    /*  
+        Simple attempt to take in a value, convert it to an IP and output the decimal value.
+        
+        Sample IP
+        2405:0205:8481:84DB:76A9:4824:523E:917C
+        216.147.214.045
+    */
+    // Read the args.  Arg 0 is always the name/path of the command
+    let args: Vec<String> = env::args().collect();
+    
+    // Check if we have our 2nd arg
+    if args.len() < 2 {
+        println!("At least one argument is required.");
+        exit(0)
+    }
+
+    // Get the first arg, which should be the IP
+    let search_ip = &args[1];
+
+    // Convert the string to an actual IP
+    let ip = IpAddr::from_str(search_ip);
+    
+    // Match on a result and do some work if OK.
+    match ip {
+            Result::Ok(val) => 
+                if val.is_ipv4() {
+                    let numeric =  u32::from(Ipv4Addr::from_str(search_ip).unwrap());
+                    println!("{:?} {}", val, numeric);
+                }
+                else {
+                    let numeric =  u128::from(Ipv6Addr::from_str(search_ip).unwrap());
+                    println!("{:?} {}", val, numeric);
+                },                
+            Result::Err(err) => println!("BAD IP {:?}", err)
+        }
+}
